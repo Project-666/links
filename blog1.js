ee1d2d2d9314d33d89fed704427b8761ed5e4564
Abengkris <105534577+abengkris@users.noreply.github.com>
2024-06-07 16:31:16 +0700

    Update blog1.js

--- blog1.js
+++ blog1.js
@@ -1,4 +1,4 @@
-function rcentbytag(e) {
+function blog1(e) {
     var ul = document.createElement("ul");
     ul.className = "recent-by-tag";
 
@@ -100,6 +100,6 @@ var showpostdate = false;
 window.onload = function () {
     var script = document.createElement("script");
     script.src =
-        "https://www.abengkris.com/feeds/posts/default?orderby=updated&alt=json-in-script&callback=rcentbytag";
+        "https://www.abengkris.com/feeds/posts/default?orderby=updated&alt=json-in-script&callback=blog2";
     document.body.appendChild(script);
 };
