a0e4367b6af67f69e1723efbb6d509c8b232c559
Abengkris <105534577+abengkris@users.noreply.github.com>
2024-07-05 20:57:35 +0700

    Update viewcount.js

--- widget/viewcount.js
+++ widget/viewcount.js
@@ -1,43 +1,9 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyBPoDYIUNaKFo0ydWHsZL8HQStpEar2rqg",
-  authDomain: "abengkriscom-1716903358301.firebaseapp.com",
-  databaseURL: "https://abengkriscom-1716903358301-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "abengkriscom-1716903358301",
-  storageBucket: "abengkriscom-1716903358301.appspot.com",
-  messagingSenderId: "427645431962",
-  appId: "1:427645431962:web:d0a44267e826deeb98af23",
-  measurementId: "G-KKGPQCXKGK"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Function to update view count
-function updateViewCount() {
-  var postId = document.querySelector('.wendy-view-post').getAttribute('data-view');
-  var pageRef = firebase.database().ref(postId);
-  pageRef.transaction(function(currentViews) {
-    return (currentViews || 0) + 1;
-  });
-}
-
-// Function to display view count
-function displayViewCount() {
-  var postId = document.querySelector('.wendy-view-post').getAttribute('data-view');
-  var pageRef = firebase.database().ref(postId);
-  pageRef.on('value', function(snapshot) {
-    var viewCount = snapshot.val() || 0;
-    var viewCountElements = document.querySelectorAll('.wendy-view-post');
-    viewCountElements.forEach(function(element) {
-      element.classList.remove('hidden');
-      element.textContent = viewCount + ' views';
+    $.each($(".post-view[data-id]"), function(a, e) {
+        var l = $(e).parent().find("#postviews").addClass("view-load"),
+            i = new Firebase("hhttps://abengkriscom-1716903358301-default-rtdb.asia-southeast1.firebasedatabase.app/pages/id/" + $(e).attr("data-id"));
+        i.once("value", function(a) {
+            var n = a.val(),
+                t = !1;
+            null == n && (n = {}, n.value = 0, n.url = window.location.href, n.id = $(e).attr("data-id"), t = !0), l.removeClass("view-load").text(n.value), n.value++, "/" != window.location.pathname && (t ? i.set(n) : i.child("value").set(n.value))
+        })
     });
-  });
-}
-
-// Load view count on page load
-window.onload = function() {
-  updateViewCount();
-  displayViewCount();
-};
