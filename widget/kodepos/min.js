a341da70b876aa4f1a39f434ab445ccab8870ea1
Abengkris <105534577+abengkris@users.noreply.github.com>
2024-08-01 23:42:06 +0700

    Update min.js

--- widget/kodepos/min.js
+++ widget/kodepos/min.js
@@ -1,8 +1,212 @@
-async function getProvinces(){try{let t=await fetch("https://links.abengkris.com/widget/kodepos/provinces.json");if(!t.ok)throw Error("Network response was not ok");let e=await t.json();return e}catch(n){return console.error("Error loading provinces:",n),[]}}async function getCities(){try{let t=await fetch("https://links.abengkris.com/widget/kodepos/cities.json");if(!t.ok)throw Error("Network response was not ok");let e=await t.json();return e}catch(n){return console.error("Error loading cities:",n),[]}}async function getDistricts(){try{let t=await fetch("https://links.abengkris.com/widget/kodepos/districts.json");if(!t.ok)throw Error("Network response was not ok");let e=await t.json();return e}catch(n){return console.error("Error loading districts:",n),[]}}async function getSubDistricts(){try{let t=await fetch("https://links.abengkris.com/widget/kodepos/subDistricts.json");if(!t.ok)throw Error("Network response was not ok");let e=await t.json();return e}catch(n){return console.error("Error loading subDistricts:",n),[]}}async function getPostalCodes(){try{let t=await fetch("https://links.abengkris.com/widget/kodepos/postalCodes.json");if(!t.ok)throw Error("Network response was not ok");let e=await t.json();return e}catch(n){return console.error("Error loading postalCodes:",n),[]}}const errContainer=document.getElementById("error"),errorMessage=document.getElementById("error-message");function handleSearch(){let t=document.getElementById("search-input").value.trim();if(t){let e=`/p/kode-pos-indonesia.html?kota=${encodeURIComponent(t)}`;window.location.href=e}else errContainer.style.display="block",errorMessage.textContent="*Masukkan nama kota untuk mencari kode pos."}async function main(){let t=document.getElementById("loading"),e=document.getElementById("city-name"),n=document.getElementById("prov-name"),r=document.getElementById("postal-codes-table"),a=document.getElementById("table"),o=document.getElementById("info"),i=document.getElementById("zipCount");try{var s,d,l;t.style.display="block";let c=await getProvinces(),y=await getCities(),g=await getDistricts(),u=await getSubDistricts(),k=await getPostalCodes();r.innerHTML="",document.getElementById("prov-count").innerHTML=c.length,document.getElementById("city-count").innerHTML=y.length,document.getElementById("dis-count").innerHTML=g.length,document.getElementById("subdis-count").innerHTML=u.length,document.getElementById("pC-count").innerHTML=k.length;let m=new URLSearchParams(window.location.search),w=m.get("kota");if(!w){errContainer.style.display="block",errorMessage.textContent="*Cari berdasarkan nama kota/kabupaten";return}let h=y.find(t=>t.city_name.toLowerCase()===w.toLowerCase());if(!h){errContainer.style.display="block",errorMessage.textContent=`*Kota/kabupaten "${w}" tidak ada di database kami`;return}let f=h.city_id,p=(s=f,g.filter(t=>t.city_id===s));p.length,p.forEach(t=>{var e;e=t.dis_id,u.filter(t=>t.dis_id===e)});let E=(d=f,k.filter(t=>t.city_id===d)),b=E.length,C=c.find(t=>t.prov_id===h.prov_id),B=C?C.prov_name:"Unknown";E.forEach((t,e)=>{let n=document.createElement("tr"),a=g.find(e=>e.dis_id===t.dis_id),o=u.find(e=>e.subdis_id===t.subdis_id);n.innerHTML=`
-                <td>${e+1}</td>
-                <td>${B}</td>
-                <td>${h.city_name}</td>
-                <td>${a?a.dis_name:"Unknown"}</td>
-                <td>${o?o.subdis_name:"Unknown"}</td>
-                <td>${t.postal_code}</td>
-            `,r.appendChild(n)}),o.style.display="block",e.textContent=h.city_name,n.textContent=B,l=`Kode Pos ${h.city_name}`,document.title=l,a.style.display="block",i.textContent=b}catch(I){console.error("Error loading data:",I);return}finally{t.style.display="none"}}window.onload=function(){document.getElementById("search-button").addEventListener("click",handleSearch)},main();const refreshButton=document.getElementById("refresh-button");refreshButton.addEventListener("click",()=>{main()});
+async function getProvinces() {
+    try {
+        const response = await fetch("https://links.abengkris.com/widget/kodepos/provinces.json");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const provinces = await response.json();
+        return provinces;
+    } catch (error) {
+        console.error("Error loading provinces:", error);
+        return [];
+    }
+}
+
+async function getCities() {
+    try {
+        const response = await fetch("https://links.abengkris.com/widget/kodepos/cities.json");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const cities = await response.json();
+        return cities;
+    } catch (error) {
+        console.error("Error loading cities:", error);
+        return [];
+    }
+}
+
+async function getDistricts() {
+    try {
+        const response = await fetch("https://links.abengkris.com/widget/kodepos/districts.json");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const districts = await response.json();
+        return districts;
+    } catch (error) {
+        console.error("Error loading districts:", error);
+        return [];
+    }
+}
+
+async function getSubDistricts() {
+    try {
+        const response = await fetch("https://links.abengkris.com/widget/kodepos/subDistricts.json");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const subDistricts = await response.json();
+        return subDistricts;
+    } catch (error) {
+        console.error("Error loading subDistricts:", error);
+        return [];
+    }
+}
+
+async function getPostalCodes() {
+    try {
+        const response = await fetch("https://links.abengkris.com/widget/kodepos/postalCodes.json");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const postalCodes = await response.json();
+        return postalCodes;
+    } catch (error) {
+        console.error("Error loading postalCodes:", error);
+        return [];
+    }
+}
+
+const errContainer = document.getElementById("error");
+const errorMessage = document.getElementById("error-message");
+
+function handleSearch() {
+    const cityName = document.getElementById("search-input").value.trim();
+    if (cityName) {
+        const newUrl = `/p/kode-pos-indonesia.html?kota=${encodeURIComponent(cityName)}`;
+        window.location.href = newUrl;
+    } else {
+        errContainer.style.display = "block";
+        errorMessage.textContent =
+            "*Masukkan nama kota untuk mencari kode pos.";
+    }
+}
+
+async function main() {
+    const loadingElement = document.getElementById("loading");
+    const cityEl = document.getElementById("city-name");
+    const provEl = document.getElementById("prov-name");
+    const postalCodesTable = document.getElementById("postal-codes-table");
+    const tableId = document.getElementById("table");
+    const infoId = document.getElementById("info");
+    const zipCount = document.getElementById("zipCount");
+
+    try {
+        loadingElement.style.display = "block";
+        const provinces = await getProvinces();
+        const cities = await getCities();
+        const districts = await getDistricts();
+        const subDistricts = await getSubDistricts();
+        const postalCodes = await getPostalCodes();
+        postalCodesTable.innerHTML = "";
+
+        function getDistrictsByCity(city_id) {
+            return districts.filter(district => district.city_id === city_id);
+        }
+
+        function getSubDistrictsByDistrict(dis_id) {
+            return subDistricts.filter(
+                subDistrict => subDistrict.dis_id === dis_id
+            );
+        }
+
+        function getPostalCodesByCity(city_id) {
+            return postalCodes.filter(
+                postalCode => postalCode.city_id === city_id
+            );
+        }
+
+        function updateTitle(newTitle) {
+            document.title = newTitle;
+        }
+
+        document.getElementById("prov-count").innerHTML = provinces.length;
+        document.getElementById("city-count").innerHTML = cities.length;
+        document.getElementById("dis-count").innerHTML = districts.length;
+        document.getElementById("subdis-count").innerHTML = subDistricts.length;
+        document.getElementById("pC-count").innerHTML = postalCodes.length;
+
+        // Mengambil parameter 'city' dari URL
+        const urlParams = new URLSearchParams(window.location.search);
+        const cityName = urlParams.get("kota");
+
+        if (!cityName) {
+            errContainer.style.display = "block";
+            errorMessage.textContent = `*Cari berdasarkan nama kota/kabupaten`;
+            return;
+        }
+
+        const city = cities.find(
+            city => city.city_name.toLowerCase() === cityName.toLowerCase()
+        );
+        if (!city) {
+            errContainer.style.display = "block";
+            errorMessage.textContent = `*Kota/kabupaten "${cityName}" tidak ada di database kami`;
+            return;
+        }
+
+        const cityId = city.city_id;
+        const districtsInCity = getDistrictsByCity(cityId);
+
+        const disNewCount = districtsInCity.length;
+
+        districtsInCity.forEach(district => {
+            const subDistrictsInDistrict = getSubDistrictsByDistrict(
+                district.dis_id
+            );
+        });
+
+        const postalCodesInCity = getPostalCodesByCity(cityId);
+        const newZipCode = postalCodesInCity.length;
+
+        const province = provinces.find(
+            province => province.prov_id === city.prov_id
+        );
+        const provinceName = province ? province.prov_name : "Unknown";
+
+        postalCodesInCity.forEach((postalCode, index) => {
+            const row = document.createElement("tr");
+            const district = districts.find(
+                district => district.dis_id === postalCode.dis_id
+            );
+            const subDistrict = subDistricts.find(
+                subDistrict => subDistrict.subdis_id === postalCode.subdis_id
+            );
+
+            row.innerHTML = `
+                <td>${index + 1}</td>
+                <td>${provinceName}</td>
+                <td>${city.city_name}</td>
+                <td>${district ? district.dis_name : "Unknown"}</td>
+                <td>${subDistrict ? subDistrict.subdis_name : "Unknown"}</td>
+                <td>${postalCode.postal_code}</td>
+            `;
+            postalCodesTable.appendChild(row);
+        });
+        infoId.style.display = "block";
+        cityEl.textContent = city.city_name;
+        provEl.textContent = provinceName;
+        updateTitle(`Kode Pos ${city.city_name}`);
+        tableId.style.display = "block";
+        zipCount.textContent = newZipCode;
+    } catch (error) {
+        console.error("Error loading data:", error);
+        return;
+    } finally {
+        loadingElement.style.display = "none";
+    }
+}
+
+window.onload = function () {
+    document
+        .getElementById("search-button")
+        .addEventListener("click", handleSearch);
+};
+
+main();
+
+const refreshButton = document.getElementById("refresh-button");
+refreshButton.addEventListener("click", () => {
+    main();
+});
