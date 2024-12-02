cd1ed5614351391e15a44df752509f18af95ce1a
Abengkris <105534577+abengkris@users.noreply.github.com>
2024-12-02 19:10:07 +0700

    Update btcidr.js

--- widget/btcidr.js
+++ widget/btcidr.js
@@ -1,4 +1,4 @@
-// Bitcoin price in IDR by Abengkris (abengkris.com)
+// Bitcoin price in IDR by Abeng
 
 async function fetchOHLC() {
     const from = Math.floor(Date.now() / 1000) - 24 * 60 * 60; // 24 hours ago
